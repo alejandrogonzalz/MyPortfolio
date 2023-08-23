6ea8a471e798fc0754b56765dc6fa5eb5776edcd
Alejandro González Almazán <alex.aga816@gmail.com>
2023-08-23 00:22:26 -0600

    switch user interface added

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import classes from "./navbar.module.scss";
 
 import { NavElements } from "./NavElements";
+import { ThemeSwitch } from "./utilities/switchers";
 
 export const Navbar = () => {
   return (
     <>
       <div className={classes.navbar}>
+        <ThemeSwitch />
         <NavElements />
       </div>
       <main>
--- src/navbar/utilities/switchers.tsx
+++ src/navbar/utilities/switchers.tsx
@@ -1,9 +1,29 @@
-// import classes from "./switcher.module.scss";
+import { useEffect, useState } from "react";
+import classes from "./switcher.module.scss";
+import * as SwitchPrimitive from "@radix-ui/react-switch";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 
-export const ThemeSwitch = () => {
+export const ThemeSwitch = ({ ...props }) => {
+  const [checked, setChecked] = useState("dark");
+  let content;
+
+  const handleChecked = () => {
+    setChecked((prevChecked) => (prevChecked === "dark" ? "light" : "dark"));
+  };
+
+  content = checked === "dark" ? <SunIcon /> : <MoonIcon />;
+
   return (
-    <>
-      <div></div>
-    </>
+    <label htmlFor={props.id} className={classes.switch_container}>
+      <SwitchPrimitive.Root
+        className={classes.switch_root}
+        id={props.id}
+        onCheckedChange={handleChecked}
+        checked={checked === "light"} // Make sure to pass the checked prop to your switch library
+      >
+        <SwitchPrimitive.Thumb className={classes.switch_thumb} />
+      </SwitchPrimitive.Root>
+      <div className={classes.icon}>{content}</div>
+    </label>
   );
 };
